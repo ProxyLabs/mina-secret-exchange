b27c9bd5f59a3f3f1469bc58ee29fc430b5d98d1
flkl192b <Florian.kluge@mailbox.tu-dresden.de>
2021-12-10 13:52:01 +0100

    add compiled

---
+++ dist/snapp/snapp.js
@@ -0,0 +1,16 @@
+import { Field, isReady, } from "snarkyjs";
+export { myFunc };
+// class MyContract extends SmartContract {
+//   @state(Field) x: State<Field>;
+// }
+async function myFunc() {
+    await setup();
+}
+async function setup() {
+    await isReady;
+    console.log(Field(3).toString());
+}
+// export { myFunc };
+// function myFunc() {
+//   console.log("hi");
+// }
